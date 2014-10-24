20ce5ea7e07cb31dd50428c9deeec9fb35b39540
Lucille Kenney <lucilleakenney@gmail.com>
2014-10-24 17:05:19 -0400

    Testing console comments week8
    Testing console comments in apps.js on github since they're not working on my desktop.

---
+++ js/apps.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('#apple-btn').click(function(){
+		console.log('The apple button was clicked!');
+		alert('We love apples!');
+	}); //end apple click
+
+    
+}); //end ready
+
+
+
+
